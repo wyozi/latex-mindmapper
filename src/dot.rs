129203740a39caa391a0bd7a31d9b25fe15094fc
wyozi <joonasmertanen@gmail.com>
2019-10-22 16:46:22 +0300

    Output digraphs rather than graphs

--- src/dot.rs
+++ src/dot.rs
@@ -3,7 +3,7 @@ use std::fmt::Write;
 
 pub fn format_dot(source_nodes: &Vec<SourceNode>) -> String {
     let mut s = String::new();
-    writeln!(&mut s, "graph mygraph {{").unwrap();
+    writeln!(&mut s, "digraph mygraph {{").unwrap();
 
     // Set each node's label
     for node in source_nodes.iter().filter(|n| n.label.is_some()) {
@@ -13,7 +13,7 @@ pub fn format_dot(source_nodes: &Vec<SourceNode>) -> String {
     // Draw links
     for node in source_nodes {
         for par_id in &node.parent_ids {
-            writeln!(&mut s, "\t{} -- {}", par_id, node.id).unwrap();
+            writeln!(&mut s, "\t{} -> {}", par_id, node.id).unwrap();
         }
     }
 
@@ -30,18 +30,18 @@ mod tests {
     fn test_format() {
         assert_eq!(format_dot(&vec!(
             SourceNode { parent_ids: vec!(), id: "foo".to_string(), label: None }
-        )), "graph mygraph {\n}");
+        )), "digraph mygraph {\n}");
         assert_eq!(format_dot(&vec!(
             SourceNode { parent_ids: vec!(), id: "foo".to_string(), label: Some("yay".to_string()) }
-        )), "graph mygraph {\n\tfoo [label=\"yay\"]\n}");
+        )), "digraph mygraph {\n\tfoo [label=\"yay\"]\n}");
         assert_eq!(format_dot(&vec!(
             SourceNode { parent_ids: vec!(), id: "foo".to_string(), label: None },
             SourceNode { parent_ids: vec!("foo".to_string()), id: "bar".to_string(), label: None },
-        )), "graph mygraph {\n\tfoo -- bar\n}");
+        )), "digraph mygraph {\n\tfoo -> bar\n}");
         assert_eq!(format_dot(&vec!(
             SourceNode { parent_ids: vec!(), id: "foo".to_string(), label: None },
             SourceNode { parent_ids: vec!(), id: "bar".to_string(), label: None },
             SourceNode { parent_ids: vec!("foo".to_string(), "bar".to_string()), id: "foobar".to_string(), label: None },
-        )), "graph mygraph {\n\tfoo -- foobar\n\tbar -- foobar\n}");
+        )), "digraph mygraph {\n\tfoo -> foobar\n\tbar -> foobar\n}");
     }
 }
