0d76c1dbbabf7b9fc7272e92b6ee01c7da96f230
wyozi <joonasmertanen@gmail.com>
2019-10-22 16:43:41 +0300

    Label should be parsed from curly braces

--- src/parser.rs
+++ src/parser.rs
@@ -8,7 +8,7 @@ pub struct SourceNode {
 }
 
 pub fn parse_string(input: &str) -> Vec<SourceNode> {
-    let re = Regex::new(r"\\lmmnode\{([^}]*)\}\{([^}]*)\}(?:\[([^\]]*)\])?").unwrap();
+    let re = Regex::new(r"\\lmmnode\{([^}]*)\}\{([^}]*)\}(?:\{([^\}]*)\})?").unwrap();
     
     re.captures_iter(input).map(|cap| {
         let parent_ids = cap[1].split(",").filter(|s| s != &"").map(|s| s.to_string()).collect();
@@ -30,16 +30,16 @@ mod tests {
         assert_eq!(parse_string(r"\lmmnode{}{b}"), vec!(
             SourceNode { parent_ids: vec!(), id: "b".to_string(), label: None }
         ));
-        assert_eq!(parse_string(r"\lmmnode{a}{b}[c]"), vec!(
+        assert_eq!(parse_string(r"\lmmnode{a}{b}{c}"), vec!(
             SourceNode { parent_ids: vec!("a".to_string()), id: "b".to_string(), label: Some("c".to_string()) }
         ));
-        assert_eq!(parse_string(r"\lmmnode{a,b}{i}[l]"), vec!(
+        assert_eq!(parse_string(r"\lmmnode{a,b}{i}{l}"), vec!(
             SourceNode { parent_ids: vec!("a".to_string(), "b".to_string()), id: "i".to_string(), label: Some("l".to_string()) }
         ));
-        assert_eq!(parse_string(r"\lmmnode{}{b}[c] \lmmnode{i}{j} \lmmnode{x}{y}[z]"), vec!(
+        assert_eq!(parse_string(r"\lmmnode{}{b}{c} \lmmnode{i}{j} \lmmnode{x,i}{y}{z}"), vec!(
             SourceNode { parent_ids: vec!(), id: "b".to_string(), label: Some("c".to_string()) },
             SourceNode { parent_ids: vec!("i".to_string()), id: "j".to_string(), label: None },
-            SourceNode { parent_ids: vec!("x".to_string()), id: "y".to_string(), label: Some("z".to_string()) }
+            SourceNode { parent_ids: vec!("x".to_string(), "i".to_string()), id: "y".to_string(), label: Some("z".to_string()) }
         ));
     }
 }
